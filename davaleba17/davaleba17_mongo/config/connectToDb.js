2a24ee71708f9de12611510c6f4357187ca1304e
Lasha-Davituliani <davituliani88@gmail.com>
2025-11-12 16:34:51 +0400

    davaleba 17 dasrulebuli

--- davaleba17/davaleba17_mongo/config/connectToDb.js
+++ davaleba17/davaleba17_mongo/config/connectToDb.js
@@ -3,7 +3,7 @@ const { default: mongoose } = require("mongoose");
 module.exports = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://admin:Admin123%21@cluster1.jck1a59.mongodb.net/TestProject?retryWrites=true&w=majority&appName=TestProject"
+      "mongodb+srv://admin:Admin123!@cluster1.jck1a59.mongodb.net/?appName=Cluster1"
     );
     console.log("connected successfully");
   } catch (error) {
