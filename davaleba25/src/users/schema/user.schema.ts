74ce092f145de8160e062fe7f2cb3d1ea1f91121
Lasha-Davituliani <davituliani88@gmail.com>
2025-12-09 13:51:13 +0400

    Added Davaleba 25

---
+++ davaleba25/src/users/schema/user.schema.ts
@@ -0,0 +1,17 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema({ timestamps: true })
+export class User {
+  @Prop({ type: String })
+  firstName: string;
+  @Prop({ type: String })
+  lastName: string;
+
+  @Prop({ type: String })
+  email: string;
+
+  @Prop({ type: Number })
+  age: number;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
