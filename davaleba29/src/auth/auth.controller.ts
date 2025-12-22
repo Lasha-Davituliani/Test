8df7638df644ed8df258cc98455b9f2d843950d5
Lasha-Davituliani <davituliani88@gmail.com>
2025-12-22 16:52:21 +0400

    davaleba 29

---
+++ davaleba29/src/auth/auth.controller.ts
@@ -0,0 +1,27 @@
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SignUpDto } from './DTO/sign-up.dto';
+import { SignInDto } from './DTO/sign-in.dto';
+import { AuthGuard } from './guards/auth.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+  @Post('sign-up')
+  signUp(@Body() signUpDto: SignUpDto) {
+    return this.authService.signUp(signUpDto);
+  }
+
+  @Post('sign-in')
+  signIn(@Body() signInDto: SignInDto) {
+    return this.authService.signIn(signInDto);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('current-user')
+  currentUser(@Req() request) {
+    const userId = request.userId;
+    const user = this.authService.currentUser(userId);
+    return user;
+  }
+}
